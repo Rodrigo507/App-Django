9298082e67b309a5ef2af7ca777aecb70d79f29f
Rodrigo Gutierrez <rodrigo.gutierrez@up.ac.pa>
2021-08-11 22:00:53 -0500

    :sparkles: Create template for polls - Template index

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # example : /polls/
     path("", views.index, name='index'),
+    # example : /polls/1
+    path('<int:question_id>/', views.detail, name='detail'),
+    # example : /polls/1/results/
+    path('<int:question_id>/results/', views.result, name='results'),
+    # example : /polls/1/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from polls.models import Question
+
+
 def index(request):
-    return HttpResponse("Hola mundo")
+    last_questions_list = Question.objects.order_by('pub_date')[:5]
+    output = ','.join([q.question_text for q in last_questions_list])
+    context = {
+        'result': output
+    }
+    return render(request, '../templates/index.html', context)
+
+
+def detail(request, question_id):
+    response = ("Estas viendo la pregunta %s." % question_id)
+    return HttpResponse(response)
+
+
+def result(request, question_id):
+    response = ("Resultado la pregunta %s." % question_id)
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    response = ("Resultado la pregunta %s." % question_id)
+    return HttpResponse(response)
